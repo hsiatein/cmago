9b34736ccb3aecc4e657b0ba09e0925c5406fe29
XiaTian <3023625451@qq.com>
2025-08-10 21:35:04 +0800

    修复add

--- src/cmago/add.rs
+++ src/cmago/add.rs
@@ -9,16 +9,21 @@ use url::Url;
 
 pub fn add(url:&str){
     let mut git_url=url.to_string();
+    let mut name=None;;
     if Url::parse(url).is_ok(){
         if !url.ends_with(".git"){
             git_url = url.to_string()+".git";
         }
     }else if let Some(external) = find_external(url){
         git_url=external.to_string();
+        name=Some(url);
+    }
+    if name.is_none(){
+        name=Some(extract_repo_name(git_url.as_str()).unwrap());
     }
     let cmago_path = find_cmago().unwrap();
     let mut cmago_toml = CmagoToml::from_path(cmago_path.to_str().unwrap());
-    cmago_toml.deps.add_dep(extract_repo_name(git_url.as_str()).unwrap(),git_url.as_str());
+    cmago_toml.deps.add_dep(name.unwrap(),git_url.as_str());
     let toml = cmago_toml.to_string();
     let mut file = File::create(cmago_path.as_path()).expect("Could not create Cmago.toml");
     file.write_all(toml.as_bytes()).expect("Could not write Cmago.toml");
