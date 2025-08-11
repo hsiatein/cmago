c8de51e873fde762099cd02981acc1f54200380d
XiaTian <3023625451@qq.com>
2025-08-11 23:13:40 +0800

    加入run相关

--- src/cmago/run.rs
+++ src/cmago/run.rs
@@ -1,10 +1,11 @@
+use std::fs::canonicalize;
 use crate::cmago::build::build;
 use crate::call_tools::run_executable;
 use crate::cmago::utils::find_cmago_dir;
 
 pub fn run(name:&str, release:bool){
     build(name,release);
-    let mut exec_path = find_cmago_dir().unwrap().join("build");
+    let mut exec_path = canonicalize(find_cmago_dir().unwrap().join("build")).expect("Can't find cmago dir");
     if cfg!(target_os = "windows") {
         exec_path = exec_path.join(format!("{name}.exe").as_str());
     } else if cfg!(target_os = "linux") {
